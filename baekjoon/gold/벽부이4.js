281d36f98d8cc8eb76f97dad8ab0ea31d4ffdaec
Hyeondoonge <migu554@naver.com>
2022-04-05 17:10:57 +0900

    [dfs] 벽부수고이동하기4 수정

--- baekjoon/gold/벽부이4.js
+++ baekjoon/gold/벽부이4.js
@@ -1,66 +1,60 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const [N, M] = input[0].split(' ').map((e) => parseInt(e));
+const group_size = {};
 const map = [];
-const visitied = Array.from(new Array(N), () => new Array(M).fill(false));
-const answer = Array.from(new Array(N), () => new Array(M).fill(0));
 
 for(let i = 0; i < N; i++) {
-  map.push(Array.from(input[i + 1]));
-
-  for(let j = 0; j < M; j++) {
-    if (map[i][j] === '1') {
-      answer[i][j] = 1;
-    }
-  }
+  map.push(Array.from(input[i + 1]).map((e) => parseInt(e)));
 }
+
+let number = 2;
 
 const dr = [-1, 1, 0, 0];
 const dc = [0, 0, -1, 1];
-let size = 0;
-let adjVertexes = [];
 
 for(let i = 0; i < N; i++) {
   for(let j = 0; j < M; j++) {
-    if (map[i][j] === '1') continue;
-    if (visitied[i][j]) continue;
-    visitied[i][j] = true;
-    dfs(i, j);
-
-    console.log(adjVertexes.length);
-    for(let k = 0; k < adjVertexes.length; k++) {
-      const {r, c} = adjVertexes[k];
-      answer[r][c] += size;
-    }
-    adjVertexes = [];
-    size = 0;
-  } 
+    if (map[i][j] !== 0) continue;
+    const size = grouping(i, j, number);
+    group_size[number++] = size;
+  }
 }
 
-console.log(answer.map((e) => e.map((n) => n % 10).join('')).join('\n'));
+const answer = Array.from(new Array(N), () => new Array(M).fill(0));
 
-function dfs (r, c) {
-  for(let v = 0; v < 4; v++) {
-    const adjR = r + dr[v];
-    const adjC = c + dc[v];
+for(let i = 0; i < N; i++) {
+  for(let j = 0; j < M; j++) {
+    if (map[i][j] !== 1) continue;
+    answer[i][j] = 1;
+    const adjNumbers = [];
+    for(let k = 0; k < 4; k++) {
+      const nr = i + dr[k];
+      const nc = j + dc[k];
+      if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
 
-    if (adjR < 0 || adjR >= N || adjC < 0 || adjC >= M) continue;
-    if (map[adjR][adjC] === '0') continue;
-
-    if (!adjVertexes.find(({ r, c }) => adjR === r && adjC === c)) {
-      adjVertexes.push({ r: adjR, c: adjC });
+      const adjNumber = map[nr][nc];
+      if (adjNumber === 1 || adjNumbers.includes(adjNumber)) continue;
+      answer[i][j] += group_size[adjNumber];
+      adjNumbers.push(adjNumber);
     }
+    answer[i][j] %= 10;
   }
-  size++;
+}
+
+console.log(answer.map((e) => e.join('')).join('\n'));
+
+function grouping (r, c, number) {
+  map[r][c] = number;
+
+  let cnt = 1;
 
   for(let k = 0; k < 4; k++) {
     const nr = r + dr[k];
     const nc = c + dc[k];
-
     if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-    if (map[nr][nc] === '1') continue;
-    if (visitied[nr][nc]) continue;
-    visitied[nr][nc] = true;
-    dfs(nr, nc);
+    if (map[nr][nc] !== 0) continue; // 이미 방문 or 방문할 수 없는 벽
+    cnt += grouping(nr, nc, number);
   }
+  return cnt;
 }

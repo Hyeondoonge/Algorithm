749fabea240675d3b36fa971b7fd40a53698fa8c
Hyeondoonge <migu554@naver.com>
2022-10-25 13:51:34 +0900

    [구현] 수강신청
    - Set API 사용한 버전

--- baekjoon/silver/수강신청.js
+++ baekjoon/silver/수강신청.js
@@ -1,15 +1,8 @@
 // 중복된 학번을 제거하면서 최종 수강신청 학번의 수가 수용가능한 인원의 수보다 적을 수 있는 걸 고려해야한다.
+// 중복제거를 직접 구현해도되지만, Set API를 이용할 수 있다.
 
 function solution(K, L, waiting_numbers) {
-  const apply_number = {};
-  const waiting_numbers_no_duplicate = [];
-
-  for (let i = L - 1; i >= 0; i--) {
-    const number = waiting_numbers[i];
-    if (apply_number[number]) continue;
-    apply_number[number] = true;
-    waiting_numbers_no_duplicate.push(number);
-  }
+  const waiting_numbers_no_duplicate = [...new Set(waiting_numbers.reverse())];
 
   let answer = '';
   const N = waiting_numbers_no_duplicate.length;

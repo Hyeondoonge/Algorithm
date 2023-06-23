24c474800485c4fe776c39ba9d7e2229d2df4488
Hyeondoonge <migu554@naver.com>
2023-06-23 18:00:50 +0900

    [연결리스트] 표 편집

--- programmers/level3/표 편집.js
+++ programmers/level3/표 편집.js
@@ -1,64 +1,97 @@
-let arr;
-
-const move = (c, num, index) => {
-  num = num * 1;
-
-  if (c === 'D') {
-    for(let i = index + 1; i < arr.length; i++) {
-      if (arr[i] === 'O') num--;
-      if (num === 0) return i;
-    }
-  } else {
-    for(let i = index - 1; i >= 0; i--) {
-      if (arr[i] === 'O') num--;
-      if (num === 0) return i;
-    }
-  }
-};
-
 function solution(n, k, cmd) {
-  arr = new Array(n).fill('O');
-  const stack = [];
-  let index = k;
+  const linkedList = {};
 
-  for(let i = 0; i < cmd.length; i++) {
-    const [c, num] = cmd[i].split(" ");
+  linkedList['head'] = { prev: null, next: null };
+  linkedList['head'].next = linkedList['tail'] = { prev: linkedList['head'], next: null };
 
-    if (c === 'D' || c === 'U') {
-      index = move(c, num, index);
-    } else if (c === 'C') {
+  for (let i = 0; i < n; i++) {
+    const node = {
+      prev: linkedList['tail'].prev,
+      next: linkedList['tail'],
+      element: i
+    };
+    linkedList['tail'].prev = linkedList['tail'].prev.next = node;
+  }
 
-      arr[index] = 'X';
-      stack.push(index);
+  let current = null;
 
-      let last = true;
+  for (let i = 0, node = linkedList['head'].next; i <= k; i++, node = node.next) {
+    current = node;
+  }
 
-      for(let i = index + 1; i < arr.length; i++) {
-        if (arr[i] === 'O') {
-          index = i;
-          last = false;
-          break;
-        }
-      }
+  const history = [];
 
-      if (last) {
-        for(let i = index - 1; i >= 0; i--) {
-          if (arr[i] === 'O') {
-            index = i;
-            break;
-          }
-        }
-      }
+  for (let i = 0; i < cmd.length; i++) {
+    const [type, param] = cmd[i].split(' ');
 
-      // 뒤에 삭제된 개수로 따지도록 변경
-
-      // 마지막인덱스인지 체크만으로 해결하지 못함.
-    } else if(c === 'Z'){
-      const idx = stack.pop();
-      arr[idx] = 'O';
+    switch (type) {
+      case 'U':
+        up(param);
+        break;
+      case 'D':
+        down(param);
+        break;
+      case 'C':
+        remove();
+        break;
+      case 'Z':
+        restore();
+        break;
     }
   }
-  console.log(arr.join(''));
-};
 
-solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]);
+  let answer = '';
+  let last = null;
+
+  for (let i = 0, node = linkedList['head'].next; node !== null; i++, node = node.next) {
+    if (isTail(node)) {
+      answer += 'X'.repeat(n - last.element - 1);
+      continue;
+    }
+
+    if (!last) {
+      answer += 'X'.repeat(node.element) + 'O';
+    } else {
+      answer += 'X'.repeat(node.element - last.element - 1) + 'O';
+    }
+    last = node;
+  }
+
+  return answer;
+
+  function up(move) {
+    move = Number(move);
+    for (let i = 0, node = current; i <= move; i++, node = node.prev) {
+      current = node;
+    }
+  }
+
+  function down(move) {
+    move = Number(move);
+    for (let i = 0, node = current; i <= move; i++, node = node.next) {
+      current = node;
+    }
+  }
+
+  function remove() {
+    history.push(current);
+    current.prev.next = current.next;
+    current.next.prev = current.prev;
+
+    if (isTail(current.next)) {
+      current = current.prev;
+    } else {
+      current = current.next;
+    }
+  }
+
+  function restore() {
+    const node = history.pop();
+    node.prev.next = node;
+    node.next.prev = node;
+  }
+
+  function isTail(node) {
+    return !node.next;
+  }
+}

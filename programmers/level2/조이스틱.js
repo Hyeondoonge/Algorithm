b9e7a5fe4c9907ba889812639c8f90da29c67f12
Hyeondoonge <migu554@naver.com>
2024-03-21 21:22:02 +0900

    [완전탐색] 조이스틱

--- programmers/level2/조이스틱.js
+++ programmers/level2/조이스틱.js
@@ -1,43 +1,53 @@
 function solution(name) {
-  const N = name.length;
+  const n = name.length
 
-  const visitied = Array.from({ length: name.length }, () => [false, false]);
-  let answer = Infinity;
-
-  dfs(0, 0);
-
-  for (let i = 0; i < N; i++) {
-    const c = name[i].charCodeAt(0);
-    answer += Math.min(Math.abs(c - 65), Math.abs(c - 90) + 1);
+  let target = 0
+  for (let i = 0; i < n; i++) {
+    if (name[i] !== 'A') target++
   }
 
-  return answer;
+  const visitied = Array(n).fill(false)
 
-  function dfs(i, d) {
-    if (
-      visitied.every((_, index) => {
-        if (name[index] === 'A') return true;
-        if (index === 0) return true;
-        let prev = index - 1 === -1 ? N - 1 : index - 1;
-        let next = index + 1 === N ? 0 : index + 1;
-        if (visitied[prev][1] || visitied[next][0]) return true;
-      })
-    ) {
-      answer = Math.min(answer, d);
+  let answer = Infinity
+  visitied[0] = true
+
+  search(0, diff(name[0]), name[0] === 'A' ? 0 : 1)
+
+  return answer
+
+  function search(idx, cnt, d) {
+    if (d === target) {
+      answer = Math.min(answer, cnt)
+      return
     }
 
-    let next = N === i + 1 ? 0 : i + 1;
-    if (!visitied[i][1]) {
-      visitied[i][1] = true;
-      dfs(next, d + 1);
-      visitied[i][1] = false;
+    for (let k = 1; k < n; k++) {
+      const next = (idx + k) % n
+      if (visitied[next] || name[next] === 'A') continue
+      visitied[next] = true
+      search(next, cnt + diff(name[next]) + k, d + 1)
+      visitied[next] = false
+      break
     }
 
-    next = -1 === i - 1 ? N - 1 : i - 1;
-    if (!visitied[i][0]) {
-      visitied[i][0] = true;
-      dfs(next, d + 1);
-      visitied[i][0] = false;
+    for (let k = 1; k < n; k++) {
+      let next = (idx - k) % n
+      if (next < 0) {
+        next = n + next
+      }
+      if (visitied[next] || name[next] === 'A') continue
+      visitied[next] = true
+      search(next, cnt + diff(name[next]) + k, d + 1)
+      visitied[next] = false
+
+      break
     }
+  }
+
+  function diff(param) {
+    const a = 'A'.charCodeAt(0)
+    const z = 'Z'.charCodeAt(0)
+
+    return Math.min(param.charCodeAt(0) - a, z - param.charCodeAt(0) + 1)
   }
 }

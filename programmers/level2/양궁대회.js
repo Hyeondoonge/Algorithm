04954c8c75ac6e9abb7070ed19eac33917823682
Hyeondoonge <migu554@naver.com>
2024-06-04 00:56:09 +0900

    [조합] 양궁대회 - 재풀이

--- programmers/level2/양궁대회.js
+++ programmers/level2/양궁대회.js
@@ -1,56 +1,63 @@
+// ‼️분할한 기능들‼️이 정상적으로 동작하는지 확인
+
+// 결승전 규칙
+// 어피치 n발 -> 라이언 n발
+// 점수 계산
+// - 10점 ~ 0점
+// - k(1~10)점을 어피치 a, 라이언 b 만큼 맞혔으면 더 많은 화살을 맞힌 선수가 k점 획득
+// 이때, a=b이면 어피치가 k점 획득. 만약 0으로 같으면 어느 누구도 k점 획득 X
+// 최종 점수 계산, 동일하면 어피치가 우승자
+
+// input
+// n (1~10)
+// info (length: 11) 어피치가 맞춘 상태
+
+// output
+// 라이언이 가장 큰 점수 차이로 이기기 위해 n발을 쏳아야하는 점수 (length: 11, 10점~0점 순서대로)
+// 이때 여러 가지 방법인 경우, 가장 낮은 점수를 더 많이 맞힌 경우를 return. 가장 큰 점수일 때까지 비교
+// 무조건 지거나 비기는 경우 -1
+
 function solution(n, info) {
-  const MAX = 11;
-  const board = Array.from({ length: MAX }, () => 0); // 10점 ~ 0점
-  const answer = [[-1], -1]; // 점수 차이
+  // 라이언 과녁 상태
+  const ryan = Array.from({ length: 11 }, () => 0)
+  const answer = [null, 0] // 배열, maxDiff
+  comb(0, n)
 
-  shoot(n, 0);
+  return answer[0] || [-1]
 
-  return answer;
+  function isRyanWin() {
+    let ryan_score = 0
+    let apeach_score = 0
 
-  function shoot(left, d) {
-    if (d === MAX) {
-      const [rian, apeach] = score();
-
-      if (rian <= apeach || rian - apeach < answer[1]) return;
-      if (answer[1] < rian - apeach) {
-        answer[0] = [...board];
-        answer[1] = rian - apeach;
-      } else {
-        for (let i = MAX - 1; i >= 0; i--) {
-          // 작은 점수부터 비교
-          if (answer[0][i] === board[i]) continue;
-          if (answer[0][i] > board[i]) return;
-          break;
+    for (let i = 0; i < 10; i++) {
+      // 10점~0점
+      if (ryan[i] <= info[i]) {
+        if (ryan[i] === info[i] && ryan[i] === 0) {
+          continue
         }
-        answer[0] = [...board];
-        answer[1] = rian - apeach;
+        apeach_score += 10 - i
+      } else {
+        ryan_score += 10 - i
       }
-      return;
     }
-
-    for (let i = n; i >= 0; i--) {
-      // 몇 발인지를 나타냄
-      if (left - i < 0) continue;
-      if (left - i > 0 && d + 1 === MAX) continue;
-      board[d] = i;
-      shoot(left - i, d + 1);
-    }
+    return [ryan_score > apeach_score, Math.abs(ryan_score - apeach_score)]
   }
 
-  function score() {
-    let rian = 0,
-      apeach = 0;
-    // 점수 구하기
-    for (let i = 0; i < MAX; i++) {
-      if (board[i] === info[i]) {
-        if (board[i] === 0) continue;
-        apeach += MAX - i - 1;
-      } else if (board[i] > info[i]) {
-        rian += MAX - i - 1;
-      } else {
-        apeach += MAX - i - 1;
+  function comb(d, left) {
+    if (10 < d) {
+      if (left === 0) {
+        const result = isRyanWin()
+        if (result[0] && answer[1] < result[1]) {
+          answer[0] = [...ryan]
+          answer[1] = result[1]
+        }
       }
+      return
     }
-    return [rian, apeach];
+
+    for (let i = left; i >= 0; i--) {
+      ryan[10 - d] = i
+      comb(d + 1, left - i)
+    }
   }
 }

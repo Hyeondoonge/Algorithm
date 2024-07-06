72195e56aa3bf8c7e20d6a58228becaec1bafb5f
Hyeondoonge <migu554@naver.com>
2024-07-07 01:34:47 +0900

    [재귀] 혼자 놀기의 달인 - 풀이 업데이트

--- programmers/level2/혼자 놀기의 달인.js
+++ programmers/level2/혼자 놀기의 달인.js
@@ -1,52 +1,25 @@
+// 다른 풀이범: union and find
+
 function solution(cards) {
   const N = cards.length
 
-  let answer = 0
+  const visitied = Array.from({ length: N }, () => false)
+  const sizes = []
 
-  for (let i = 1; i <= N; i++) {
-    for (let j = 1; j <= N; j++) {
-      answer = Math.max(getPoint(i, j), answer)
-    }
+  for (let i = 0; i < N; i++) {
+    let size = getLinkSize(i)
+    sizes.push(size)
   }
+  sizes.sort((a, b) => b - a)
 
-  return answer
+  return sizes[0] * sizes[1]
 
-  function getPoint(i, j) {
-    const p = Array.from({ length: N + 1 }, (_, index) => index)
-
-    // 1번째 상자
-    let c = i
-    let A = 1
-
-    while (find(c) !== find(cards[c - 1])) {
-      union(c, cards[c - 1])
-      A++
-      c = cards[c - 1]
+  function getLinkSize(i) {
+    if (visitied[i]) {
+      return 0
     }
-
-    if (find(i) === find(j)) return 0
-
-    // 2번째 상자
-    c = j
-    let B = 1
-
-    while (find(c) !== find(cards[c - 1])) {
-      union(c, cards[c - 1])
-      B++
-      c = cards[c - 1]
-    }
-
-    return A * B
-
-    function union(u, v) {
-      const pu = find(u)
-      const pv = find(v)
-      p[pu] = p[pv]
-    }
-
-    function find(u) {
-      if (p[u] === u) return u
-      return (p[u] = find(p[u]))
-    }
+    visitied[i] = true
+    const next = cards[i] - 1
+    return 1 + getLinkSize(next)
   }
 }
